03db330257c25a3cd97e50cab2b4a9c85660af86
Akashyadav-96 <yadavakash0031@gmail.com>
2026-07-30 10:49:28 +0530

    initial commit

---
+++ lab2/fun.js
@@ -0,0 +1,22 @@
+const F1 = () => {
+  console.log("F1");
+};
+const F2 = () => {
+  console.log("F2");
+};
+const F3 = () => {
+  console.log("F3");
+};
+const F4 = () => {
+  console.log("F4");
+};
+
+function main() {
+  console.log("main");
+  F1();
+  F2();
+  F3();
+  F4();
+  console.log("End");
+}
+main();
